5e02c04425a90dc3a1aa8b9556f11f9ed15b4187
Ridireacht <ridiredire@gmail.com>
2023-03-31 00:48:21 +0300

    Add User model

---
+++ NoteApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace NoteApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
