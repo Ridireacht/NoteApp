6f0ae3e83792c69c764230a4e7c2073a212b1223
Ridireacht <ridiredire@gmail.com>
2023-03-31 00:58:14 +0300

    Add DbContext for models

---
+++ NoteApp/Models/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace NoteApp.Models
+{
+    public class Context : DbContext
+    {
+        // для тестирования работы EF
+        public Context()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+
+        // настройка
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=.\App_Data\NoteUser.sqlite3");
+        }
+
+
+        // таблицы, которые будут map'иться в БД
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
