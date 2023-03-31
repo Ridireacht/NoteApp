f9b7dae5cd8304211d847df6bcc04d50bc308951
Ridireacht <ridiredire@gmail.com>
2023-03-31 13:54:40 +0300

    Remove unnecessary stuff

--- NoteApp/Controllers/HomeController.cs
+++ NoteApp/Controllers/HomeController.cs
@@ -2,17 +2,11 @@
 using NoteApp.Models;
 using System.Diagnostics;
 
+
 namespace NoteApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -21,12 +15,6 @@ namespace NoteApp.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
