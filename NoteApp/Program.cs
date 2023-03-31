ff35e239bc0573ba64e82a5a80088fb6bd78d221
Ridireacht <ridiredire@gmail.com>
2023-03-31 14:04:34 +0300

    Remove unnecessary stuff

--- NoteApp/Program.cs
+++ NoteApp/Program.cs
@@ -13,11 +13,6 @@ namespace NoteApp
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
             app.UseStaticFiles();
 
             app.UseRouting();
