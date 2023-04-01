e1825aab8c487651c22c34d9d5328e6193e69b18
Ridireacht <ridiredire@gmail.com>
2023-04-01 12:58:22 +0300

    Style, redirect to main page

--- NoteApp/Program.cs
+++ NoteApp/Program.cs
@@ -4,24 +4,24 @@ namespace NoteApp
     {
         public static void Main(string[] args)
         {
+            // Add services to the container
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
-
             builder.Services.AddDbContext<Models.Context>();
 
+
+            // App setup
             var app = builder.Build();
 
             app.UseStaticFiles();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Account}/{action=SignIn}");
+                pattern: "{controller=Home}/{action=Index}");
 
             app.Run();
         }
