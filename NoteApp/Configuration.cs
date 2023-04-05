951c09fd997bc766a734869dd3f9e764e90c513b
Ridireacht <ridiredire@gmail.com>
2023-04-05 22:36:16 +0300

    Updated configuration

--- NoteApp/Configuration.cs
+++ NoteApp/Configuration.cs
@@ -1,4 +1,6 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
 
 
 namespace NoteApp
@@ -9,13 +11,62 @@ namespace NoteApp
         public static IEnumerable<ApiScope> ApiScopes =>
             new List<ApiScope>
             {
-                new ApiScope();
+                new ApiScope("sample-scope")
             };
 
 
         // Области на сервере представлены ресурсами - Identity и API.
-        // Identity-ресурс моделирует область, позволяющая приложению
+        // Identity-ресурс моделирует область, позволяющая приложению видеть утверждения (claim'ы) о пользователе
         public static IEnumerable<IdentityResource> IdentityResources =>
-            new List<IdentityResource> { }
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+
+        // API-ресурс моделирует доступ ко всему ресурсу
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                new ApiResource("sample-api-resource", new [] { JwtClaimTypes.Name})
+                {
+                    Scopes = {"sample-scope"}
+                }
+            };
+
+
+        // Clients - список приложений с определёнными правами, которые могут использовать систему
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "sample-id",
+                    ClientName = "sample-name",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequireClientSecret = false,
+                    RequirePkce = true,
+                    RedirectUris =
+                    {
+                        "http://.../signin-oidc"
+                    },
+                    AllowedCorsOrigins =
+                    {
+                        "http://..."
+                    },
+                    PostLogoutRedirectUris =
+                    {
+                        "http://.../signout-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "sample-scope"
+                    },
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
     }
 }
