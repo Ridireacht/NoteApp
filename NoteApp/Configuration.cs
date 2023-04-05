d5306a619281f207b0b6d702979fc158cc21a076
Ridireacht <ridiredire@gmail.com>
2023-04-05 22:22:51 +0300

    Basic configuration file for IdentityServer

---
+++ NoteApp/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.Models;
+
+
+namespace NoteApp
+{
+    public class Configuration
+    {
+        // ApiScope описывает то, что приложению можно использовать (доступ к областям)
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope();
+            };
+
+
+        // Области на сервере представлены ресурсами - Identity и API.
+        // Identity-ресурс моделирует область, позволяющая приложению
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource> { }
+    }
+}
