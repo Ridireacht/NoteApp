988538baa31aee37549cc9523425524e6a4ff39b
Ridireacht <ridiredire@gmail.com>
2023-04-07 16:10:53 +0300

    On model creating method

--- NoteApp/Models/AuthDbContext.cs
+++ NoteApp/Models/AuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -17,6 +18,23 @@ namespace NoteApp.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source=.\App_Data\NoteUser.sqlite3");
+        }
+
+
+        //
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<User>(entity => entity.ToTable(name: "Users"));
+            builder.Entity<IdentityRole>(entity => entity.ToTable(name: "Roles"));
+            builder.Entity<IdentityUserRole<string>>(entity => entity.ToTable(name: "UserRoles"));
+            builder.Entity<IdentityUserClaim<string>>(entity => entity.ToTable(name: "UserClaims"));
+            builder.Entity<IdentityUserLogin<string>>(entity => entity.ToTable(name: "UserLogins"));
+            builder.Entity<IdentityUserToken<string>>(entity => entity.ToTable(name: "UserTokens"));
+            builder.Entity<IdentityRoleClaim<string>>(entity => entity.ToTable(name: "RoleClaims"));
+
+            builder.ApplyConfiguration(new UserConfiguration());
         }
 
 
