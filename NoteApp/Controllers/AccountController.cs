f347fe8aee31a45e62cef324e8affc1f34028ea4
Ridireacht <ridiredire@gmail.com>
2023-04-07 16:51:38 +0300

    Updated controller to work with SignIn

--- NoteApp/Controllers/AccountController.cs
+++ NoteApp/Controllers/AccountController.cs
@@ -1,10 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using NoteApp.Models;
 
 namespace NoteApp.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly IIdentityServerInteractionService _interactionService;
+
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager, IIdentityServerInteractionService interactionService)
+            => (_signInManager, _userManager, _interactionService) = (signInManager, userManager, interactionService);
+
+
         [HttpGet]
         public IActionResult SignIn(string returnUrl)
         {
@@ -14,8 +25,29 @@ namespace NoteApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult SignIn(SignInViewModel viewModel)
+        public async Task<IActionResult> SignIn(SignInViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var user = await _userManager.FindByNameAsync(viewModel.Username);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "User not found");
+                return View(viewModel);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Username, viewModel.Password, false, false);
+            
+            if (result.Succeeded)
+            {
+                return Redirect(viewModel.ReturnUrl);
+            }
+
+            ModelState.AddModelError(string.Empty, "Login error");
+
             return View(viewModel);
         }
 
