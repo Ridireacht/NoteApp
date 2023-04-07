54be9b26ce21abf9582b7ff91596f6ea97e1818a
Ridireacht <ridiredire@gmail.com>
2023-04-07 16:01:54 +0300

    Correct configuration

--- NoteApp/Models/UserConfiguration.cs
+++ NoteApp/Models/UserConfiguration.cs
@@ -3,11 +3,11 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace NoteApp.Models
 {
-    public class UserConfiguration : IEntityTypeConfiguration<UserConfiguration>
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-
+            builder.HasKey(x => x.ID);
         }
     }
 }
