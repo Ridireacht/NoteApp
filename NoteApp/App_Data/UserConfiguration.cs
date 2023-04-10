95b35c8b7467b5029bf1c0e6366a0c286f639283
Ridireacht <ridiredire@gmail.com>
2023-04-10 17:31:48 +0300

    Comment fix

--- NoteApp/App_Data/UserConfiguration.cs
+++ NoteApp/App_Data/UserConfiguration.cs
@@ -10,7 +10,7 @@ namespace NoteApp.App_Data
         public void Configure(EntityTypeBuilder<User> builder)
         {
             // Настраиваем отношение между таблицами.
-            // У каждого User может быть много Note, связь между ними: User.UserName + Note.UserName
+            // У каждого User может быть много Note, связь между ними: User.Id + Note.UserId
             builder.HasMany(o => o.Notes).WithOne().HasForeignKey(d => d.UserId).IsRequired();
         }
     }
