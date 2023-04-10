34346c67e64e896a496861e2fe9c73acd1fa1dd6
Ridireacht <ridiredire@gmail.com>
2023-04-10 14:41:18 +0300

    Add comments

--- NoteApp/App_Data/UserConfiguration.cs
+++ NoteApp/App_Data/UserConfiguration.cs
@@ -2,12 +2,15 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using NoteApp.Models;
 
+
 namespace NoteApp.App_Data
 {
     public class UserConfiguration : IEntityTypeConfiguration<User>
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            // Настраиваем отношение между таблицами.
+            // У каждого User может быть много Note, связь между ними: User.ID + Note.UserID
             builder.HasMany(o => o.Notes).WithOne().HasForeignKey(d => d.UserID).IsRequired();
         }
     }
