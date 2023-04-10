374a9afca654ebb7ccc686ba2e3edea87fef0931
Ridireacht <ridiredire@gmail.com>
2023-04-11 00:58:19 +0300

    Add Authorize

--- NoteApp/Controllers/HomeController.cs
+++ NoteApp/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace NoteApp.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
+        [Authorize]
         public IActionResult Index()
         {
             return View();
