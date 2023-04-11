c41d6144c75e58743a3dfd58af8376822d468351
Ridireacht <ridiredire@gmail.com>
2023-04-11 14:57:01 +0300

    Keep on testing

--- NoteApp/Controllers/HomeController.cs
+++ NoteApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NoteApp.Models;
 
 
 namespace NoteApp.Controllers
@@ -7,9 +8,14 @@ namespace NoteApp.Controllers
     public class HomeController : Controller
     {
         [HttpGet]
-        [Authorize]
+        //[Authorize]
         public IActionResult Index()
         {
+            var viewModel = new HomeViewModel();
+            viewModel.Username = "test";
+
+            ViewData["Info"] = viewModel;
+
             return View();
         }
     }
