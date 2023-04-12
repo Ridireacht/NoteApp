6b659b6c7d58cbdc838546ec2247955b8a6ac7c1
Ridireacht <ridiredire@gmail.com>
2023-04-12 11:46:39 +0300

    Change main file to work with Startup.cs

--- NoteApp/Program.cs
+++ NoteApp/Program.cs
@@ -1,9 +1,5 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using NoteApp.App_Data;
-using NoteApp.Models;
-using System.Net;
+
 
 namespace NoteApp
 {
@@ -11,109 +7,36 @@ namespace NoteApp
     {
         public static void Main(string[] args)
         {
-            // Добавляем сервисы в контейнер
-            var builder = WebApplication.CreateBuilder(args);
+			// Создаём builder
+			var host = CreateHostBuilder(args).Build();
 
-            builder.Services.AddDbContext<AuthDbContext>(options =>
-            {
-                options.UseSqlite(@"Data Source=.\App_Data\NoteUser.sqlite3");
-            });
+			// Создаём scope и проверяем, существует ли БД
+			using (var scope = host.Services.CreateScope())
+			{
+				var serviceProvider = scope.ServiceProvider;
+				try
+				{
+					var context = serviceProvider.GetRequiredService<AuthDbContext>();
+					DbInitializer.Initialize(context);
+				}
 
-            builder.Services.AddIdentity<User, IdentityRole>(config =>
-            {
-                config.Password.RequiredLength = 8;
-                config.Password.RequireNonAlphanumeric = false;
-                config.Password.RequireDigit = true;
-                config.Password.RequireUppercase = false;
-                config.Password.RequireLowercase = false;
-                config.Password.RequiredUniqueChars = 0;
-            })
-                .AddEntityFrameworkStores<AuthDbContext>()
-                .AddDefaultTokenProviders();
+				catch (Exception exception)
+				{
+					var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
+					logger.LogError(exception, "An error occurred while app initialization");
+				}
+			}
 
-            builder.Services.AddIdentityServer()
-                .AddAspNetIdentity<User>()
-                .AddInMemoryApiResources(Configuration.ApiResources)
-                .AddInMemoryIdentityResources(Configuration.IdentityResources)
-                .AddInMemoryApiScopes(Configuration.ApiScopes)
-                .AddInMemoryClients(Configuration.Clients)
-                .AddDeveloperSigningCredential();
+			host.Run();
+		}
 
-            builder.Services.ConfigureApplicationCookie(config =>
-            {
-                config.Cookie.Name = "Notes.Identity.Cookie";
-                config.LoginPath = "/Account/SignIn";
-                config.LogoutPath = "/Account/SignOut";
-            });
-
-            // Аутентификация в приложении, конфигурация 
-            builder.Services.AddAuthentication(config =>
-            {
-                config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer("Bearer", options =>
-                {
-                    options.Authority = "http://localhost:25798/";
-                    options.Audience = "NoteApp";
-                    options.RequireHttpsMetadata = false;
-                });
-
-			builder.Services.AddControllersWithViews();
-			builder.Services.AddRazorPages();
-
-
-
-			// Настраиваем приложение
-			var app = builder.Build();
-
-            // Если не авторизован и попал на сторонние страницы, то редирект
-            // на страницу входа
-            app.UseStatusCodePages(async context =>
-            {
-                var response = context.HttpContext.Response;
-
-                if (response.StatusCode == (int)HttpStatusCode.Unauthorized ||
-                        response.StatusCode == (int)HttpStatusCode.Forbidden)
-                    response.Redirect("/Account/SignIn");
-            });
-
-
-            app.UseStaticFiles();
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseIdentityServer();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapDefaultControllerRoute();
-            });
-
-            app.MapRazorPages();
-
-
-
-            // Проверяем, существует ли БД
-            using (var scope = app.Services.CreateScope())
-            {
-                var serviceProvider = scope.ServiceProvider;
-                try
-                {
-                    var context = serviceProvider.GetRequiredService<AuthDbContext>();
-                    DbInitializer.Initialize(context);
-                }
-
-                catch (Exception ex)
-                {
-                    var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while DB creation!");
-                }
-            }
-
-
-            app.Run();
-        }
-    }
+		
+		// Используем настройки из Startup.cs
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
