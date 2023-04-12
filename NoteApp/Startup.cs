b0d4716121bd686ce34735397cdaf9911431df33
Ridireacht <ridiredire@gmail.com>
2023-04-12 11:41:02 +0300

    Remove unnecessary usings

--- NoteApp/Startup.cs
+++ NoteApp/Startup.cs
@@ -1,11 +1,4 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.FileProviders;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;	
 using System.Security.Claims;
 using NoteApp.App_Data;
