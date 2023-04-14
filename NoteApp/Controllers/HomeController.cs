fdd3799ec0009d8678df59996ce55ba550be3f86
Ridireacht <ridiredire@gmail.com>
2023-04-14 21:15:25 +0300

    Add (kinda) functional Create method

--- NoteApp/Controllers/HomeController.cs
+++ NoteApp/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NoteApp.App_Data;
 using NoteApp.Models;
 using System.Security.Claims;
 
@@ -25,7 +26,32 @@ namespace NoteApp.Controllers
 		[Authorize]
 		public IActionResult CreateNote()
 		{
-			return View();
+			int note_id;
+
+
+			// Заносим в БД новую (пустую заметку)
+			using (var cxt = new AuthDbContext(default))
+			{
+				Note nt = new Note()
+				{
+					UserId = User.FindFirst(ClaimTypes.NameIdentifier).Value,
+					Title = "",
+					Content = "",
+					CreationDate = DateTime.Now,
+					LastModified = DateTime.Now
+				};
+
+				cxt.Notes.Add(nt);
+				cxt.SaveChanges();
+
+
+				// Запоминаем ID новой записи
+				note_id = nt.Id;
+			}
+
+
+			// Переходим к новосозданной заметке
+			return Redirect($"~Home/Notes/{note_id}");
 		}
 	}
 }
