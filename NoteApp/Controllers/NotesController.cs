cd02100ace65d4c444cc309e82c43d4741b647e2
Ridireacht <ridiredire@gmail.com>
2023-04-14 21:40:01 +0300

    Add (kinda) functional Delete

--- NoteApp/Controllers/NotesController.cs
+++ NoteApp/Controllers/NotesController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using NoteApp.App_Data;
+using NoteApp.Models;
+using System.Security.Claims;
 
 // Проверяем, есть ли у этого пользователя заметка с таким ID.
 // Если её нет вовсе или она принадлежит другому, то кидаем 404.
@@ -33,6 +35,15 @@ namespace NoteApp.Controllers
 		[Authorize]
 		public IActionResult DeleteNote(int note_id)
 		{
+			// Удаляем заметку по её ID
+			using (var cxt = new AuthDbContext(default))
+			{
+				Note nt = new Note() { Id = note_id };
+
+				cxt.Notes.Remove(nt);
+				cxt.SaveChanges();
+			}
+
 			return View();
 		}
 	}
