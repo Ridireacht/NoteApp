9a958ade2c5f5a7022f662528d8989233a62f1c2
Ridireacht <ridiredire@gmail.com>
2023-04-14 21:44:52 +0300

    Add boilerplate ViewModel for individual Note

---
+++ NoteApp/Models/NoteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NoteApp.Models
+{
+	public class NoteViewModel
+	{
+		public int Id { get; set; }
+
+		public string? Title { get; set; }
+		public string? Content { get; set; }
+
+		public DateTime CreationDate { get; set; }
+		public DateTime LastModified { get; set; }
+	}
+}
