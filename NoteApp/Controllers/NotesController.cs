0dbd1bd4675189f90444062cfd20c54673395d3a
Ridireacht <ridiredire@gmail.com>
2023-04-14 20:33:30 +0300

    Add controller for notes

---
+++ NoteApp/Controllers/NotesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace NoteApp.Controllers
+{
+	public class NotesController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
